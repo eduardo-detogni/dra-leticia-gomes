d3a1031ed472d6fbc38ab45936f8c0b6d146dfcb
eduardo-detogni <detognieduardo@gmail.com>
2025-05-11 10:32:46 -0300

    olá mundo

---
+++ assets/javascripts/script.js
@@ -0,0 +1,21 @@
+function openOverlay1() {
+    // Exibir o overlay
+    document.getElementById("overlay1").classList.add("show");
+}
+
+function openOverlay2() {
+    // Exibir o overlay
+    document.getElementById("overlay2").classList.add("show");
+}
+
+function openOverlay3() {
+    // Exibir o overlay
+    document.getElementById("overlay3").classList.add("show");
+}
+
+function closeOverlay() {
+    // Ocultar o overlay
+    document.getElementById("overlay1").classList.remove("show");
+    document.getElementById("overlay2").classList.remove("show");
+    document.getElementById("overlay3").classList.remove("show");
+}
